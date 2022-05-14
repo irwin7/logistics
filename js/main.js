a8a447f8969b3efecfbafe991b8d0e2a425e114d
irwins7 <irwins1337@gmail.com>
2022-05-14 14:43:59 +0500

    modal done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+const elModal = document.querySelector('.modal');
+const elModalClose = document.querySelector('.modal__menu-btn');
+const elModalOpen = document.querySelector('.header__menu-btn');
+
+
+elModalOpen.addEventListener('click',()=>{
+    elModal.classList.add('modal--active');
+})
+elModalClose.addEventListener('click',()=>{
+    elModal.classList.remove('modal--active')
+})
+
+// Swiper
 var swiper = new Swiper(".mySwiper", {
     effect: "cards",
     grabCursor: true,
